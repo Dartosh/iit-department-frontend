71e5d62b58550ec3d653250a0ccd7a96d690e7b6
Dartosh <dartoshka21@gmail.com>
2021-12-12 19:05:16 +0300

    feat: implement token create

--- iit-department-frontend/src/components/posts/Posts.tsx
+++ iit-department-frontend/src/components/posts/Posts.tsx
@@ -1,24 +1,73 @@
 import React, { Component } from "react";
 import './Posts.css';
+import axios from "axios";
+
+const HEROKU_APP = 'https://cors-anywhere.herokuapp.com/';
+const BASE_PATH = 'http://127.0.0.1:8000/auth/jwt/create/';
+const SEARCH_PATH = '/search';
+const SEARCH_PARAM = 'query=';
+
+
 
 class Posts extends Component<any, any> {
+    state = {
+        searchQuery: '',
+        result: {},
+    }
+
+
+
+    componentDidMount() {
+        const { searchQuery } = this.state;
+
+        axios({
+            method: 'post',
+            url: BASE_PATH,
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            data: {
+                "username": "admin",
+                "password": "admin"
+            }
+        })
+            .then(res => this.setPosts(res))
+            .catch(error => console.log(error));
+    }
+
+    setPosts = ( result: any ): void => {
+        const { data } = result;
+        const { access } = data;
+
+        axios.get("http://127.0.0.1:8000/api/v1/feed/", {
+            headers: {
+                "accept": "application/json",
+                "Authorization": `Bearer ${access}`
+            }
+        })
+
+        //this.setState({ result });
+    }
+
+
     render() {
-        const posts: number[] = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] ;
+        const { searchQuery, result } = this.state;
+        const { hits = [] }: any = result;
 
         return(
             <main>
                 <h3>POSTS</h3>
-                {posts.map(({ numb, index }: any) => (
+                {hits.map(({ id, create_date, user, text, comments_count }: any) => (
                     <article className="post">
-                        <h4>Title</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, ipsum?</p>
+                        <h4>{id}</h4>
+                        <p>{text}</p>
                         <div className="post__info">
-                            <span>Created at</span>
-                            <span>Author</span>
+                            <span>{create_date}</span>
+                            <span>{user}</span>
                         </div>
                     </article>
                 ))}
-
             </main>
         )
     }
