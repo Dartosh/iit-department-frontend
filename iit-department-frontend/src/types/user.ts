d86f19c3833e28cead4e287744e9c3b92d9df2b9
Antony <56368902+Dartosh@users.noreply.github.com>
2021-12-17 00:32:33 +0300

    feat: implement main types interfaces

---
+++ iit-department-frontend/src/types/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    email: string;
+    status: string;
+    avatar: string;
+    lastName: string;
+    firstName: string;
+    phone: string;
+    roles: Array<string>;
+}
