62773fc593dce2ce90023b7aa54e123224118aaf
a.klintsou <a.klintsou@tapston.com>
2022-05-09 13:01:12 +0300

    feat: set mock workers and watchers

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,9 +1,16 @@
-export function* workerSaga() {
+import { takeEvery, put, call } from 'redux-saga/effects';
 
+import { GET_LATEST_NEWS } from '../constants';
+import { getLatestNews } from "../../api";
+import { setLatestNews } from '../actions/actionCreator';
+
+export function* handleLatestNews(): Object {
+    const { hits } = yield call(getLatestNews);
+    yield put(setLatestNews(hits));
 }
 
 export function* watchClickSaga() {
-
+    yield takeEvery(GET_LATEST_NEWS, handleLatestNews);
 }
 
 export default function* rootSaga() {
