b69c23e66ee4f3052bce16f519c6d2b4161164cb
Anton Shnip <toni.mr.anton@mail.ru>
2022-10-18 00:06:19 +0300

    feature: add profile page with modal window

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import classNames from "classnames";
+
+import './Button.css';
+
+
+const Button: React.FC<buttonProps> = ({
+    children,
+    onClick,
+    className,
+    disabled,
+    active
+}) => {
+
+    const classes: string = classNames(
+        className,
+        { active },
+    )
+
+
+
+    return(
+        <button
+            className={classes}
+            disabled={disabled}
+            onClick={onClick}
+        >{children}
+        </button>
+    )
+}
+
+type buttonProps = {
+    children: React.ReactNode,
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    className: string,
+    disabled: boolean,
+    active: boolean,
+}
+
+export default Button;
