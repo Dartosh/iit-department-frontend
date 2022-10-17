d36562be4f334cb1671605a0e14f2ef2dc8f41b9
Dartosh <Dartoshka21@gmail.com>
2022-10-17 23:33:13 +0300

    refactor: add provider into index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import Library from './containers/library/Library';
+import Auth from './containers/auth/Auth';
+import Profile from './containers/profile/Profile';
+import Posts from './containers/posts/Posts';
 import { setupStore } from './redux/store';
 
 const store = setupStore();
@@ -14,16 +20,18 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
-              <App>
-                  <Routes>
-                      <Route path='/' element={<Auth />}/>
-                      <Route path='/library' element={<Library />}/>
-                      <Route path='/posts' element={<Posts />}/>
-                      <Route path='/profile' element={<Profile />}/>
-                  </Routes>
-              </App>
-  </BrowserRouter>
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App>
+          <Routes>
+              <Route path='/' element={<Auth />}/>
+              <Route path='/library' element={<Library />}/>
+              <Route path='/posts' element={<Posts />}/>
+              <Route path='/profile' element={<Profile />}/>
+          </Routes>
+      </App>
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
