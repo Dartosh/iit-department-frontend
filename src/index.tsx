6f83f3f8f852720c304418e1227bd97e2af1c1c2
Dartosh <Dartoshka21@gmail.com>
2022-10-17 23:44:35 +0300

    refactor: fix merge conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,37 @@
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router';
-import Auth from './containers/auth/Auth';
-import Library from './containers/library/Library';
-import Posts from './containers/posts/Posts';
-import Profile from './containers/profile/Profile';
 import { BrowserRouter } from 'react-router-dom';
 
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import App from './App';
+import Library from './containers/library/Library';
+import Auth from './containers/auth/Auth';
+import Profile from './containers/profile/Profile';
+import Posts from './containers/posts/Posts';
+import { setupStore } from './redux/store';
 
-const app = (
-      <BrowserRouter>
-              <App>
-                  <Routes>
-                      <Route path='/' element={<Auth />}/>
-                      <Route path='/library' element={<Library />}/>
-                      <Route path='/posts' element={<Posts />}/>
-                      <Route path='/profile' element={<Profile />}/>
-                  </Routes>
-              </App>
-      </BrowserRouter>
+const store = setupStore();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
 
-ReactDOM.render(app, document.getElementById('root')
+root.render(
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App>
+          <Routes>
+              <Route path='/' element={<Auth />}/>
+              <Route path='/library' element={<Library />}/>
+              <Route path='/posts' element={<Posts />}/>
+              <Route path='/profile' element={<Profile />}/>
+          </Routes>
+      </App>
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
